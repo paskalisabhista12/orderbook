43739da355c16d7d0b82c07b825ed99b88311eb0
Paskalis Abhista Bagaskara Yustiyanto <paskalis226@gmail.com>
2025-08-28 20:25:00 +0700

    feat: Add more dependency and implement Order Model

---
+++ apps/backend/src/main/java/com/orderbook/backend/model/Order.java
@@ -0,0 +1,16 @@
+package com.orderbook.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    public enum Side { BUY, SELL }
+    
+    private Side side;
+    private double price;
+    private int quantity;
+}
