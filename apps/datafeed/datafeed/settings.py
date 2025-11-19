929566b19ff0c7424de18a6969402e2efd39ee2c
Paskalis Abhista Bagaskara Yustiyanto <paskalis226@gmail.com>
2025-11-19 20:21:40 +0700

    feat: Allow cors header

--- apps/datafeed/datafeed/settings.py
+++ apps/datafeed/datafeed/settings.py
@@ -36,12 +36,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.auth",
+    "corsheaders",
     "apps.core",
     "apps.scraper",
     "rest_framework",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -50,6 +52,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 
 ROOT_URLCONF = "datafeed.urls"
 
